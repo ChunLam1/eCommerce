fa12a0d72b1fa1d95fb5c638f75aba74a6b69a3c
Alan <tr08td3@protonmail.ch>
2022-01-16 13:49:55 +0100

    corrected seeds & modelId

--- models/Product.model.js
+++ models/Product.model.js
@@ -1,23 +1,21 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model } = require("mongoose")
 
-const productSchema = new Schema(
-    {
-    name: {
-        type: String,
-        required: true
-      },
-      description: {
-        type: String
-      },
-      price: {
-        type: Number
-      },
-    
-});
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  price: {
+    type: Number
+  },
+  modelId: {
+    type: String
+  }
+})
 
+const Product = model("Product", productSchema)
 
-
-const Product = model("Product", productSchema);
-
-module.exports = Product;
-
+module.exports = Product
