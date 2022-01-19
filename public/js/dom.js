284a4094f6f9625342ab5dc69b507c96ebb1ceab
Tom <eizandev@gmail.com>
2022-01-19 12:23:37 +0100

    correction

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,13 +1,15 @@
 const prix = document.querySelectorAll(".price");
 const nombre = document.querySelectorAll(".quant");
 const totalPrice = document.querySelectorAll(".totalPrice");
-const btn = document.querySelector(".btnPrice")
+const btn = document.querySelector(".btnPrice");
 
-function changePrice(price,quantity){
-  return price.forEach(prices => {
-    console.log(prices.textContent,"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-    
-  })
-};
+function changePrice(price, quantity) {
+  return price.forEach((prices) => {
+    console.log(
+      prices.textContent,
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    );
+  });
+}
 
- changePrice(prix,nombre)
+changePrice(prix, nombre);
