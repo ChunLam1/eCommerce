df1a6b144c9242100af1f3492f7dfef747857802
Tom <eizandev@gmail.com>
2022-01-19 13:03:44 +0100

    cart price total done

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,15 +1,16 @@
-const prix = document.querySelectorAll(".price");
-const nombre = document.querySelectorAll(".quant");
-const totalPrice = document.querySelectorAll(".totalPrice");
+const prices = document.querySelectorAll(".price");
+const quantities = document.querySelectorAll(".quant");
+const subTotal = document.querySelectorAll(".subTotal");
 const btn = document.querySelector(".btnPrice");
+const totalPrice = document.getElementById("totalPrice");
 
-function changePrice(price, quantity) {
-  return price.forEach((prices) => {
-    console.log(
-      prices.textContent,
-      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-    );
-  });
+function calculatePrice() {
+  let total = 0;
+  for (let i = 0; i < prices.length; i++) {
+    subTotal[i].innerHTML = prices[i].innerHTML * quantities[i].innerHTML;
+    total += prices[i].innerHTML * quantities[i].innerHTML;
+  }
+  totalPrice.innerHTML = total;
 }
 
-changePrice(prix, nombre);
+window.onload = calculatePrice;
